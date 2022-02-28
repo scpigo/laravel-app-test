a780e6499d524b930e8903aa46958a1b18ae8e41
Maxim Rastorguev <maximiliyan99@mail.ru>
2022-02-28 18:22:44 +0300

    comment

--- app/Modules/posts_metrics/src/Services/Implems/PostsMetricsMongoService.php
+++ app/Modules/posts_metrics/src/Services/Implems/PostsMetricsMongoService.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Arr;
 
 class PostsMetricsMongoService implements PostsMetricsInterface {
     public function search(array $params) {
+        //PostsMetric::filter()
         $data = PostsMetric::where('group_type', Arr::get($params, 'group_type'))
             ->orderBy(Arr::get($params, 'sort'), Arr::get($params, 'sortType'))
             ->paginate(10, ['*'], 'page');
